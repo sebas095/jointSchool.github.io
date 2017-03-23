5dd9b8807e434e83d9890ce4734a68b7109c207a
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-03-23 01:58:09 -0500

    add component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { ScoreComponent } from './components/score/score.component';
+import { GameComponent } from './components/game/game.component';
+import { ChallengeComponent } from './components/challenge/challenge.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent,
+    ProfileComponent,
+    ResetPasswordComponent,
+    ScoreComponent,
+    GameComponent,
+    ChallengeComponent,
+    EditProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
