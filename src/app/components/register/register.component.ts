6492ed232619b42dd7f51e8a898d3445cf10cb2a
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-03-27 21:53:39 -0500

    fix register form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFire } from 'angularfire2';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  message: string = '';
+  user: IUser = {
+    firstname: '',
+    lastname: '',
+    username: '',
+    gender: 'f',
+    email: '',
+    password: ''
+  };
 
-  constructor() { }
+  constructor(private af: AngularFire, private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    this.auth.signupUser(this.user.email, this.user.password)
+      .then(user => {
+        this.af.database.object(`/users/${user.uid}`).set({
+          uid: user.uid,
+          firstname: this.user.firstname,
+          lastname: this.user.lastname,
+          username: this.user.username,
+          email: this.user.email,
+          gender: this.user.gender
+        });
+
+        this.router.navigate(['/login']);
+      }).catch(err => {
+        this.message = err.message;
+        this.router.navigate(['/user/create']);
+      });
+  }
+}
+
+interface IUser {
+  firstname: string;
+  lastname: string;
+  username: string;
+  gender: string;
+  email: string;
+  password: string;
 }
