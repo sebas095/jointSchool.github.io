048d8a3fbf8b33563058f469ca003372cd346a58
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-03-28 03:08:50 -0500

    challenge basic structure

--- src/app/components/challenge/challenge.component.ts
+++ src/app/components/challenge/challenge.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import { Router }  from '@angular/router';
 
 @Component({
   selector: 'app-challenge',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./challenge.component.css']
 })
 export class ChallengeComponent implements OnInit {
+  users: FirebaseListObservable<any>;
+  newChallenge: boolean = false;
+  message: string = '';
 
-  constructor() { }
+  constructor(private af: AngularFire, private router: Router) {
+    this.users = this.af.database.list('/users');
+  }
 
   ngOnInit() {
   }
 
+  createChallenge() {
+    this.newChallenge = !this.newChallenge;
+  }
 }
