6705bc296f894e8dc028dd1bf07a23ada8c1b366
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-03-29 03:36:26 -0500

    minor changes

--- src/app/components/score/score.component.ts
+++ src/app/components/score/score.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-score',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScoreComponent implements OnInit {
 
-  constructor() { }
+  constructor(private af: AngularFire) { }
 
   ngOnInit() {
   }
