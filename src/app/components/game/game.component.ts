28130cff17f438be0ac9e6cb7dfd4909073bd91c
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-03-28 23:31:14 -0500

    game basic structure

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
+  play: boolean = false;
+  seconds: number = 10;
+  sound: any;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  start() {
+    setInterval(() => this.tick(), 1000);
+    this.play = true;
+    this.sound = new Audio();
+    this.sound.src = "/assets/media/clock.mp3";
+    this.sound.load();
+    this.sound.play();
+  }
+
+  win() {
+    this.sound = new Audio();
+    this.sound.src = "/assets/media/win.mp3";
+    this.sound.load();
+    this.sound.play();
+  }
+
+  lose() {
+    this.sound = new Audio();
+    this.sound.src = "/assets/media/lose.mp3";
+    this.sound.load();
+    this.sound.play();
+  }
+
+  private tick() {
+    if (--this.seconds <= 0) {
+      this.seconds = 0;
+      this.sound.pause();
+    }
+  }
 }
