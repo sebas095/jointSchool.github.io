8e93dd886ed154d30e6e2e5221c808adee620852
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-03-30 01:20:54 -0500

    set colors charts

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -12,10 +12,12 @@ export class ProfileComponent implements OnInit {
   // Doughnut
   doughnutChartLabels1: string[] = ['Victorias', 'Derrotas'];
   doughnutChartData1: number[] = [2, 3];
+  doughnutChartColors1: any[] = [{backgroundColor: ['#6ab8de', '#f5da95']}];
   doughnutChartType: string = 'doughnut';
 
   doughnutChartLabels2: string[] = ['Fácil', 'Intermedio', 'Difícil'];
   doughnutChartData2: number[] = [4, 2, 1];
+  doughnutChartColors2: any[] = [{backgroundColor: ['#a3eba2', '#89f7f1', '#c191f1']}];
 
   constructor(private af: AngularFire, public auth: AuthService, private router: Router) { }
 
